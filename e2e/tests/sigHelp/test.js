90a3feb2e70da9903887ee28b613c42a09cea75d
Matt Bierner <matb@microsoft.com>
2018-06-29 16:16:16 -0700

    Adding basic test for response

--- e2e/tests/sigHelp/test.js
+++ e2e/tests/sigHelp/test.js
@@ -9,11 +9,25 @@ describe('SignatureHelp', () => {
     it('should not return signature help of outer template function', () => {
         const server = createServer(__dirname, 'echo-plugin');
         openMockFile(server, mockFileName, 'const q = test`abcdefg`');
-        server.send({ command: 'signatureHelp', arguments: { file: mockFileName, offset: 17, line: 1, entryNames: ['a'] } });
+        server.send({ command: 'signatureHelp', arguments: { file: mockFileName, offset: 17, line: 1 } });
 
         return server.close().then(() => {
             const sigHelpResponse = getFirstResponseOfType('signatureHelp', server).body;
             assert.strictEqual(sigHelpResponse.items.length, 0);
+        });
+    });
+
+    it('should return basic signature', () => {
+        const server = createServer(__dirname, 'echo-plugin');
+        openMockFile(server, mockFileName, 'const q = test`a(b`');
+        server.send({ command: 'signatureHelp', arguments: { file: mockFileName, offset: 18, line: 1 } });
+
+        return server.close().then(() => {
+            const sigHelpResponse = getFirstResponseOfType('signatureHelp', server).body;
+            assert.strictEqual(sigHelpResponse.items.length, 1);
+
+            const firstItem = sigHelpResponse.items[0];
+            assert.strictEqual(firstItem.prefixDisplayParts[0].text, 'b');
         });
     });
 });
