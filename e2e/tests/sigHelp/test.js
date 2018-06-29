50ca07e81dc61075e3ea14d13662a1bb6ee3592c
Matt Bierner <matb@microsoft.com>
2018-06-29 16:20:21 -0700

    Removing duplicated test setup code

--- e2e/tests/sigHelp/test.js
+++ e2e/tests/sigHelp/test.js
@@ -7,23 +7,31 @@ const mockFileName = 'main.ts';
 
 describe('SignatureHelp', () => {
     it('should not return signature help of outer template function', () => {
-        const server = createServer(__dirname, 'echo-plugin');
-        openMockFile(server, mockFileName, 'const q = test`abcdefg`');
-        server.send({ command: 'signatureHelp', arguments: { file: mockFileName, offset: 17, line: 1 } });
-
-        return server.close().then(() => {
-            const sigHelpResponse = getFirstResponseOfType('signatureHelp', server).body;
+        return getSigHelpInMockFile(
+            'const q = test`abcdefg`',
+            { offset: 17, line: 1 }
+        ).then(sigHelpResponse => {
             assert.strictEqual(sigHelpResponse.items.length, 0);
         });
     });
 
     it('should return basic signature', () => {
-        const server = createServer(__dirname, 'echo-plugin');
-        openMockFile(server, mockFileName, 'const q = test`a(b`');
-        server.send({ command: 'signatureHelp', arguments: { file: mockFileName, offset: 18, line: 1 } });
+        return getSigHelpInMockFile(
+            'const q = test`a(b`',
+            { offset: 18, line: 1 }
+        ).then(sigHelpResponse => {
+            assert.strictEqual(sigHelpResponse.items.length, 1);
 
-        return server.close().then(() => {
-            const sigHelpResponse = getFirstResponseOfType('signatureHelp', server).body;
+            const firstItem = sigHelpResponse.items[0];
+            assert.strictEqual(firstItem.prefixDisplayParts[0].text, 'b');
+        });
+    });
+
+    it('should return basic signature', () => {
+        return getSigHelpInMockFile(
+            'const q = test`a(b`',
+            { offset: 18, line: 1 }
+        ).then(sigHelpResponse => {
             assert.strictEqual(sigHelpResponse.items.length, 1);
 
             const firstItem = sigHelpResponse.items[0];
@@ -32,3 +40,13 @@ describe('SignatureHelp', () => {
     });
 });
 
+
+function getSigHelpInMockFile(fileContents, position) {
+    const server = createServer(__dirname, 'echo-plugin');
+    openMockFile(server, mockFileName, fileContents);
+    server.send({ command: 'signatureHelp', arguments: { file: mockFileName, ...position } });
+
+    return server.close().then(() => {
+        return getFirstResponseOfType('signatureHelp', server).body;
+    });
+}
